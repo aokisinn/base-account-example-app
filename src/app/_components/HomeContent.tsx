5b83bd3b7656b130257e62a60c9717ee83a29a25
aokisinn <a.sinn.dev@gmail.com>
2025-09-10 13:38:30 +0900

    不要なファイル削除

---
+++ src/app/_components/HomeContent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function HomeContent() {
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh'
+    }}>
+      <div style={{ textAlign: 'center' }}>
+        TODO: ホームコンテンツ
+      </div>
+    </div>
+  )
+}
